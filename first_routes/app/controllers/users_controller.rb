79350a07d89ede2f856234f3976aedd9d0e39eb0
Edward Bai <edwardbai31@gmail.com>
2017-10-23 14:33:26 -0700

    Add first routes

---
+++ first_routes/app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    # render json: user_params
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 404
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if User.delete(params[:id])
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
